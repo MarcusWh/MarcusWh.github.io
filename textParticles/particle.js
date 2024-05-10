57356f85015ba43d23e58819284f0dc2dd325a0e
MarcusWh <108171846+MarcusWh@users.noreply.github.com>
2024-05-10 21:20:02 +0100

    Update particle.js

--- textParticles/particle.js
+++ textParticles/particle.js
@@ -1,11 +1,10 @@
 class Particle {
   constructor(x, y, i) {
-    this.x = width / -2 + 100;
-    this.y = height / -2 + 100;
+    this.x = width / -2;
+    this.y = height / -2;
     this.tx = x;
     this.ty = y;
     this.i = i;
-    // this.counter=0;
     this.speed = this.chooseSpeed();
   }
   draw() {
@@ -14,15 +13,8 @@ class Particle {
       this.x = this.x + (this.tx - this.x) / this.speed;
       this.y = this.y + (this.ty - this.y) / this.speed;
     }
-    // if (counter>particles.length+360){
-    //     this.counter=0;
-    //     this.x=random(width)-width/2;
-    //     this.y=random(height)-height/2;
-    //     this.speed=this.chooseSpeed();
-    // }
   }
   chooseSpeed() {
-    // return random(20)+1;
     return 20;
   }
 }
