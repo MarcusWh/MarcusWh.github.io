f062590651a85daa2f494bc92773ba540da2acd1
MarcusWh <108171846+MarcusWh@users.noreply.github.com>
2024-05-10 08:27:01 +0100

    finished fractal tree

---
+++ fractalTree/sketch.js
@@ -0,0 +1,64 @@
+// let leftAngSlider;
+// let rightAngSlider;
+let length = document.getElementById("lineLength").value;
+let ltAngle = document.getElementById("leftAngle").value / 100;
+let rtAngle = document.getElementById("rightAngle").value / 100;
+let ltRatio = document.getElementById("leftBranch").value / 100;
+let rtRatio = document.getElementById("rightBranch").value / 100;
+let div = document.getElementById("canvasContainer");
+
+function setup() {
+  let wid = div.offsetWidth;
+  if (wid > 800) {
+    wid = 800;
+  }
+  let canv = createCanvas(wid, (wid * 2) / 3);
+  canv.parent(div);
+  stroke(255);
+  strokeWeight(1);
+  noFill();
+}
+
+function updateLength(value) {
+  length = value;
+}
+
+function updateLeftAngle(value) {
+  ltAngle = value / 100;
+}
+
+function updateRightAngle(value) {
+  rtAngle = value / 100;
+}
+
+function updateLeftBranch(value) {
+  ltRatio = value / 100;
+}
+
+function updateRightBranch(value) {
+  rtRatio = value / 100;
+}
+
+function draw() {
+  background(19, 42, 56);
+  rect(0, 0, width, height);
+  makeBranch(width / 2, height * 0.9, (height / 400) * length, 0);
+}
+
+function makeBranch(startX, startY, length, angle) {
+  if (length > 2) {
+    let endX = startX + length * sin(angle);
+    let endY = startY - length * cos(angle);
+    line(startX, startY, endX, endY);
+    makeBranch(endX, endY, length * ltRatio, angle + ltAngle);
+    makeBranch(endX, endY, length * rtRatio, angle + rtAngle);
+  }
+}
+
+function windowResized() {
+  let wid = div.offsetWidth;
+  if (wid > 800) {
+    wid = 800;
+  }
+  resizeCanvas(wid, (wid * 2) / 3);
+}
